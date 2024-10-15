5bc7ee9c3f9bf23192d04ebf69212b62dd3d260b
Luka Shashiashvili <shashiashvililuka1@gmail.com>
2024-10-15 23:27:09 +0400

    update/mainPhoto

--- src/app/content/infoSecond/infoSecond.tsx
+++ src/app/content/infoSecond/infoSecond.tsx
@@ -13,7 +13,6 @@ export default function infoSecond(){
                     <h2>RA80 HIGH COLOUR RENDERING INDEX</h2>
                     <h3>Allows you to see the true colour</h3>
                 </div>
-                
                 <p>The colour rendering index of the screen light is as high as RA80 which can trully show the natural color of the object and avoid the bad impression caused by color cast</p>
                 <Link href={''}>
                     <Button title={'GET OPTILITE PRO NOW ⚡'}></Button>
