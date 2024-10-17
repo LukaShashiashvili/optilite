8de56aa1efbc377a966d011b9cb56f87739f5e9b
Luka Shashiashvili <shashiashvililuka1@gmail.com>
2024-10-17 04:15:47 +0400

    update/logos

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -6,7 +6,7 @@ export default function header(){
     return(
         <header className={styles.container}>
             <div className={styles.left}>
-                <Image src={'/delivery.svg'} alt="" width={50} height={50}></Image>
+                <Image src={'/delivery.png'} alt="" width={60} height={40}></Image>
                 <div>
                     <span className={styles.diff}>
                         FREE SHIPPING
@@ -18,7 +18,7 @@ export default function header(){
                 <Image src={'/logo.png'} alt="" width={120} height={65}></Image>
             </Link>
             <div className={styles.right}>
-                <Image src={'/guarantee.png'} alt="" width={40} height={40}></Image>
+                <Image src={'/guarantee.png'} alt="" width={50} height={40}></Image>
                 <div>
                     <span className={styles.diff}>30 DAY </span>100% MONEY
                     <div>BACK GUARANTEE</div>
