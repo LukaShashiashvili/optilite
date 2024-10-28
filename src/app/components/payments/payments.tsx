0a9e63bf8cb2a8973bd6e108da8bbf8980b3b3e7
Luka Shashiashvili <154319427+LukaShashiashvili@users.noreply.github.com>
2024-10-28 18:05:07 +0400

    update

--- src/app/components/payments/payments.tsx
+++ src/app/components/payments/payments.tsx
@@ -4,11 +4,11 @@ import styles from './payments.module.css';
 export default function PaymentMethods(){
     return(
         <div className={styles.container}>
-            <Image src={'/icons/payments/amex.svg'} alt={'American Express'} width={50} height={50}></Image>
-            <Image src={'/icons/payments/dinersclub.svg'} alt={'DinersClub'} width={50} height={50}></Image>
-            <Image src={'/icons/payments/discover.svg'} alt={'Discover'} width={50} height={50}></Image>
-            <Image src={'/icons/payments/mastercard.svg'} alt={'Master Card'} width={50} height={50}></Image>
-            <Image src={'/icons/payments/visa.svg'} alt={'Visa'} width={50} height={50}></Image>
+            <Image src={'/icons/payments/amex.svg'} alt={'American Express'} width={35} height={35}></Image>
+            <Image src={'/icons/payments/dinersclub.svg'} alt={'DinersClub'} width={35} height={35}></Image>
+            <Image src={'/icons/payments/discover.svg'} alt={'Discover'} width={35} height={35}></Image>
+            <Image src={'/icons/payments/mastercard.svg'} alt={'Master Card'} width={35} height={35}></Image>
+            <Image src={'/icons/payments/visa.svg'} alt={'Visa'} width={35} height={35}></Image>
         </div>
     )
 }
