06cb260790ed1cf09c16ed414accf0c8a3b296fc
Luka Shashiashvili <shashiashvililuka1@gmail.com>
2024-12-15 20:53:00 +0400

    meta fb

---
+++ src/app/components/meta/FacebookPixel.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+
+const FacebookPixel: React.FC = () => {
+  useEffect(() => {
+    // Inject the Facebook Pixel script
+    const script = document.createElement('script');
+    script.async = true;
+    script.innerHTML = `!function(f,b,e,v,n,t,s){
+      if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '462922339832330');
+      fbq('track', 'PageView');`;
+    document.head.appendChild(script);
+
+    // Inject the no-script fallback
+    const noscript = document.createElement('noscript');
+    noscript.innerHTML = `<img height="1" width="1" style="display:none" 
+      src="https://www.facebook.com/tr?id=462922339832330&ev=PageView&noscript=1" />`;
+    document.body.appendChild(noscript);
+
+    return () => {
+      // Cleanup to prevent duplicate script injection
+      document.head.removeChild(script);
+      document.body.removeChild(noscript);
+    };
+  }, []);
+
+  return null; // This component does not render any visible UI
+};
+
+export default FacebookPixel;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import styles from "./page.module.css";
 import Header from "./header/header";
 import Content from "./content/content";
 import Footer from "./footer/footer";
+import FacebookPixel from "./components/meta/FacebookPixel";
 
 export default function Home() {
   return (
     <main className={styles.container}>
+      <FacebookPixel></FacebookPixel>
       <Header></Header>
       <Content></Content>
       <Footer></Footer>
